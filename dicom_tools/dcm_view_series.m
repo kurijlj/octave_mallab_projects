3cef76072e9d7b4d4382431e2f6440035c1574f0
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2022-03-01 18:36:51 +0100

    Snc Dosimetry: For function 'dcm_view_series' implemented slice ordering algorithm.

--- dicom_tools/dcm_view_series.m
+++ dicom_tools/dcm_view_series.m
@@ -25,28 +25,47 @@ function dcm_view_series()
 
     endif;
 
-    series_data = {};
     series_info = {};
+    series_data = {};
+    slice_location = [];
+    slice_order = [];
 
-    index = 3;
+    % Initialize GUI elements
+    graphics_toolkit qt;
+
+    i = 3;
     progress_window = waitbar( ...
         0.0, ...
         'Loading DICOM data ...', ...
         'name', 'DICOM Series View' ...
         );
-    while(length(series_file) >= index)
-        path = fullfile(series_dir, series_file{index});
+    while(length(series_file) >= i)
+        path = fullfile(series_dir, series_file{i});
         if(isdicom(path))
-            series_info = {series_info{:} dicominfo(path)};
-            series_data = {series_data{:} dicomread(path)};
+            info = dicominfo(path);
+            if( ...
+                    ismember('ImageOrientationPatient', fieldnames(info)) ...
+                    && ismember('ImagePositionPatient', fieldnames(info)) ...
+                    )
+                series_info = {series_info{:} info};
+                series_data = {series_data{:} dicomread(path)};
+                position = [info.ImagePositionPatient];
+                orientation = [info.ImageOrientationPatient];
+                norm = cross(orientation(1:3), orientation(4:end));
+                slice_location = [slice_location dot(norm, position)];
+
+            else
+                printf('%s: WARNING! File: \"%s\" ignored. No spatial info.\n', fname, path);
+
+            endif;
 
         else
             printf('%s: WARNING! Not a DICOM file: \"%s\".\n', fname, path);
 
         endif;
 
-        waitbar(index/length(series_file), progress_window);
-        index = index + 1;
+        waitbar(i/length(series_file), progress_window);
+        i = i + 1;
 
     endwhile;
 
@@ -63,8 +82,26 @@ function dcm_view_series()
 
     endif;
 
-    hfig = figure('name', 'DICOM Series View', 'units', 'points');
-    hax = axes('parent', hfig);
+    % Determine the slice order
+    i = 1;
+    slice_location_sorted = sort(slice_location);
+    while(length(series_info) >= i)
+        [res, ind] = ismember(slice_location(i), slice_location_sorted);
+        slice_order(ind) = i;
+        i = i + 1;
+    endwhile;
+
+    hfig = figure( ...
+        'name', 'DICOM Series View', ...
+        'units', 'points' ...
+        );
+    guisz = get(hfig, 'position');
+    hax = axes( ...
+        % 'parent', hfig ...
+        'parent', hfig, ...
+        'units', 'points', ...
+        'position', [0.05*guisz(3) 0.15*guisz(4) 0.90*guisz(3) 0.80*guisz(4)] ...
+        );
     hslsel = uicontrol( ...
         'parent', hfig, ...
         'style', 'slider', ...
@@ -72,24 +109,35 @@ function dcm_view_series()
         'tag', 'slice_selector', ...
         'tooltipstring', 'Select slice', ...
         'callback', @dcm_update_series_view, ...
-        'min', 1, 'max', length(series_data), ...
+        'min', 1, 'max', length(series_info), ...
         'sliderstep', [ ...
-            1/(length(series_data) - 1), ...
-            1/(length(series_data) - 1) ...
+            1/(length(series_info) - 1), ...
+            1/(length(series_info) - 1) ...
             ], ...
-        'value', 1 ...
+        % 'value', 1 ...
+        'value', 1, ...
+        % 'position', [0.05 0.05 0.90 0.20] ...
+        'position', [0.05*guisz(3) 0.05*guisz(4) 0.90*guisz(3) 0.05*guisz(4)] ...
         );
 
     % Calculate index of slice in the middle of the series and display it
-    midsl_index = ceil(length(series_data)/2);
+    msi = ceil(length(series_data)/2);
     blevel ...
-        = series_info{midsl_index}.WindowCenter ...
-        - series_info{midsl_index}.WindowWidth/2;
+        = series_info{slice_order(msi)}.WindowCenter ...
+        - series_info{slice_order(msi)}.WindowWidth/2;
+    class_min = double(intmin(class(series_data{1})));
+    if(class_min > blevel)
+        blevel = class_min;
+    endif;
     tlevel ...
-        = series_info{midsl_index}.WindowCenter ...
-        + series_info{midsl_index}.WindowWidth/2;
-    set(hslsel, 'value', midsl_index);
-    imshow(series_data{midsl_index}, [blevel, tlevel], 'parent', hax);
+        = series_info{slice_order(msi)}.WindowCenter ...
+        + series_info{slice_order(msi)}.WindowWidth/2;
+    class_max = double(intmax(class(series_data{1})));
+    if(class_max < tlevel)
+        tlevel = class_max;
+    endif;
+    set(hslsel, 'value', msi);
+    imshow(series_data{slice_order(msi)}, [blevel, tlevel], 'parent', hax);
 
     % Save GUI state
     hgui = guihandles(hfig);
@@ -98,6 +146,7 @@ function dcm_view_series()
     hgui.hslsel = hslsel;
     hgui.series_data = series_data;
     hgui.series_info = series_info;
+    hgui.slice_order = slice_order;
     hgui.itf = [blevel tlevel];
     guidata(hfig, hgui);
 
@@ -110,7 +159,8 @@ function dcm_update_series_view(obj)
     switch(gcbo)
         case {hgui.hslsel}
             slindex = round(get(hgui.hslsel, 'value'));
-            imshow(hgui.series_data{slindex}, hgui.itf, 'parent', hgui.hax_view);
+            srindex = hgui.slice_order(slindex);
+            imshow(hgui.series_data{srindex}, hgui.itf, 'parent', hgui.hax_view);
 
     endswitch;
 
