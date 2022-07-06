bd7154974c99427dbd094767cfb2482c990a087d
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2022-07-06 12:31:03 +0200

    SNC Dosimetry: Started implementation of the data validation routines for the 'Item View' controller.

--- gui_elements/mvc_framework/item_view_controller.m
+++ gui_elements/mvc_framework/item_view_controller.m
@@ -21,7 +21,34 @@ source('./item_view.m');
 %       ui_handles;
 % where 'layout; is a structure containing 'padding_px', 'row_height_px', and
 % 'btn_width_px' fields, and 'ui_handles' is a structure containing field
-% 'main_container' with a handle to the 'Item View' container.
+% 'main_container' with a handle to the 'Item View' container:
+%
+%
+%        +===================+
+%        | parent_controller |
+%        +===================+
+%        | layout            | -----+
+%        +-------------------+      |
+%        | ui_handles        | -+   |
+%        +-------------------+  |   |
+%                               |   |
+%                               |   |  +===========================+
+%                               |   +->| layout                    |
+%                               |      +===========================+
+%                               |      | padding_px                |
+%                               |      +---------------------------+
+%                               |      | row_height_px             |
+%                               |      +---------------------------+
+%                               |      | btn_width_px              |
+%                               |      +---------------------------+
+%                               |
+%                               |
+%                               |      +===========================+
+%                               +----->| ui_handles                |
+%                                      +===========================+
+%                                      | main_container (uihandle) |
+%                                      +---------------------------+
+%
 %
 % -----------------------------------------------------------------------------
 function controller = newItemViewController(item, parent_controller)
@@ -35,7 +62,7 @@ function controller = newItemViewController(item, parent_controller)
     % Validate input arguments ------------------------------------------------
 
     % Validate number of input arguments
-    if(1 ~= nargin && 2 ~= nargin)
+    if(1 ~= nargin && 4 ~= nargin)
         error( ...
             'Invalid call to %s.  Correct usage is:\n%s\n%s', ...
             fname, ...
@@ -46,7 +73,10 @@ function controller = newItemViewController(item, parent_controller)
     endif;
 
     controller = struct();
+
     if(1 == nargin)
+        % User did not supply parent controller and is running 'Item View' as
+        % standalone GUI app. Create supporting data structures
         controller.parent = struct();
 
         % Define common layout parameters
@@ -62,7 +92,38 @@ function controller = newItemViewController(item, parent_controller)
             'menubar', 'none' ...
             );
 
+    else
+        % Check if parent controller has all the required fields
+        if(~isstruct(parent_controller))
+            error('%s: parent_controller must be a data structure', fname);
+        endif;
+        if(~isfield(parent_controller, 'layout'))
+            error('%s: layout field is missing in the parent_controller structure', fname);
+        endif;
+        if(~isfield(parent_controller, 'ui_handles'))
+            error('%s: ui_handles field is missing in the parent_controller structure', fname);
+        endif;
+        if(~isstruct(parent_controller.layout))
+            error('%s: layout must be a data structure', fname);
+        endif;
+        if(~isfield(parent_controller.layout, 'padding_px'))
+            error('%s: padding_px field is missing in the layout structure', fname);
+        endif;
+        if(~isfield(parent_controller.layout, 'row_height_px'))
+            error('%s: row_height_px field is missing in the layout structure', fname);
+        endif;
+        if(~isfield(parent_controller.layout, 'btn_width_px'))
+            error('%s: btn_width_px field is missing in the layout structure', fname);
+        endif;
+        if(~isstruct(parent_controller.ui_handles))
+            error('%s: ui_handles must be a data structure', fname);
+        endif;
+        if(~isfield(parent_controller.ui_handles, 'main_container'))
+            error('%s: main_container field is missing in the ui_handles structure', fname);
+        endif;
+
     endif;
+
     controller.item = item;
     controller = newItemView(controller);
 
