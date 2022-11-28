60557e5bf4f2cd462fb3634c1eae487f3495a787
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2022-11-28 21:20:29 +0100

    G15 Dosimetry: Implemented 'find_profile_cax' and 'reset_to_calc_cax' functions for the 'Beam Profile CAX Detection' project.

--- work_in_progress/Beam Profile CAX Detection/reset_to_calc_cax.m
+++ work_in_progress/Beam Profile CAX Detection/reset_to_calc_cax.m
@@ -1,13 +1,14 @@
-function [lE, rE] = find_profile_edges(x, Dp)
+function [xR, DpR] = reset_to_calc_cax(x, Dp)
 % -----------------------------------------------------------------------------
 %
-% Function 'find_profile_edges':
+% Function 'reset_to_calc_cax':
 %
 % Use:
-%       -- [lE, rE] = find_profile_edges(x, Dp)
+%       -- [xR, DpR] = reset_to_calc_cax(x, Dp)
 %
 % Description:
-%       Find field edges from the 1D beam profile.
+%       Calculate beam profile CAX by numerically finding bea profile edges and
+%       then remap input data to the calculated postion of the CAX.
 %
 %       x  1D array representing detector positions in milimeters in the water
 %          tank during beam scanning procedure.
@@ -16,7 +17,7 @@ function [lE, rE] = find_profile_edges(x, Dp)
 %          positions in the water tank during beam scanning procedure.
 %
 % -----------------------------------------------------------------------------
-    fname = 'find_profile_edges';
+    fname = 'reset_to_calc_cax';
     use_case_a = sprintf('-- [lE, rE] = %s(x, Dp)', fname);
 
     % Validate number of passed arguments
@@ -82,60 +83,12 @@ function [lE, rE] = find_profile_edges(x, Dp)
 
     endif;
 
-    % Calculate smalest detector step from the array of detector positions
-    dstep = min(arrayfun(@(x, y) y-x, x(1:end-1, 1), x(2:end, 1)));
+    % Calculate profile CAX
+    [lE, rE] = find_profile_edges(x, Dp);
+    CAX = find_profile_cax(lE, rE);
 
-    % Calculate step for the data resampling
-    rstep = dstep/4;
-
-    % Resample imput data to get smoother numerical derivates
-    xR   = x(1):rstep:x(end);
-    DpR  = interp1(x, Dp, xR, 'spline');
-
-    % Normalize profile data if not normalized
-    if(100.00 ~= max(DpR))
-        DpR = DpR * 100 / max(DpR);
-
-    endif;
-
-    % Calculate the first derivate of the data
-    DpD1 = zeros(size(DpR));
-    idx = 2;
-    while(numel(DpR)-1 >= idx)
-        DpD1(idx) = (DpR(idx+1) - DpR(idx-1))/(2*rstep);
-
-        ++idx;
-
-    endwhile;
-
-    % Assume that endpoints of the first derivates array does not differ
-    % significantly from the neighboring points
-    DpD1(1) = DpD1(2);
-    DpD1(end) = DpD1(end - 1);
-
-    % Segment left penumbra
-    lROI = find(DpD1 >= 0.2*(max(DpD1)));
-
-    % Fit Gaussian to fist derivates of the left penumbra
-    c = polyfit(xR(lROI), log(DpD1(lROI)), 2);
-
-    % Center of Gaussian represnets penumbra's mid point
-    lE = -0.5*c(2)/c(1);
-
-    % Segment right penumbra
-    rROI = find(DpD1 < 0.2*(min(DpD1)));
-
-    % Fit Gaussian
-    c = polyfit(xR(rROI), log(abs(DpD1(rROI))), 2);
-
-    % Calculate right field edge
-    rE = -0.5*c(2)/c(1);
-
-    % Plot results
-    % plot(xR, DpR, xR, DpD1);
-    % hold on;
-    % plot([lE, lE], [min(DpD1)*2, max(DpR)*1.2], 'linestyle', '--', 'color', 'red');
-    % plot([rE, rE], [min(DpD1)*2, max(DpR)*1.2], 'linestyle', '--', 'color', 'red');
-    % hold off;
+    % Remap given profile data to the calculated CAX
+    xR  = x - CAX;
+    DpR = Dp;
 
 endfunction;
